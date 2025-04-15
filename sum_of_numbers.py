ae27d3e6b8cfe9889595d3402c9777db51ff2648
Amara Tie <amara.tie@stu.ocsb.ca>
2025-04-15 21:27:00 +0000

    sum_of_numbers.py

--- sum_of_numbers.py
+++ sum_of_numbers.py
@@ -10,20 +10,25 @@
 def main():
 
     # Ask User for a number
-    user_as_string = int(input("Enter a positive number :"))
+    user_as_string = input("Enter a positive number :")
     print("")
 
     # Initialize counter
     loop_counter = 0
     sum = 0
-    # Initialize counter
+    
+    # Use a while loop to add all the integers from 0
     try:
         user_as_number = int(user_as_string)
-        while loop_counter < user_as_number:
-            print("{} THE TIME OF LOOP.".format(loop_counter))
-            loop_counter = loop_counter + 1
-            sum = loop_counter + sum
-        print("The sum is {}".format(sum))
+        # Check if the number is positive 
+        if user_as_number > 0: 
+            while loop_counter < user_as_number:
+                print("{} THE TIME OF LOOP.".format(loop_counter))
+                loop_counter = loop_counter + 1
+                sum = loop_counter + sum
+            print("The sum is {}".format(sum))
+        else:
+            print("Please enter a positive number")
     except Exception:
         print("This is not a number")
     finally:
